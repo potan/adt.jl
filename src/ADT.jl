28fbd3a7690355800dfd9e52e82c49c655e846e6
Mike Potanin <mpotanin@gmail.com>
2014-11-09 19:50:07 +0400

    Refactoring

--- src/ADT.jl
+++ src/ADT.jl
@@ -3,33 +3,30 @@
   if(length(p) == 0)
    c
   else
-   (r,d) = p[1]
-   n1 = pcomp(c,np,p[2:end])
-   if(r == :(=))
-    :(let $d ; $n1 ; end)
-   elseif(r == :(==))
-    :(if $(d) ; $n1 else $np end)
-   elseif(r == :(m))
-    d(n1)
-   end
+   p[1](pcomp(c,np,p[2:end]))
  end
 
  casev(v,np,e1) = let
-  spat(n::Symbol, p::Symbol) = (:(=), :($p = $v.$n))
-  spat(n::Symbol, p::Expr) = (:(m), let s = gensym() ; (m) ->
+  spat(n::Symbol, p) = let s = gensym() ; (m) ->
             :(let
                $s = $v.$n
                $(casev(s,np,:($p->$m)))
             end)
-           end)
-  spat(n::Symbol, p::Any) = (:(==), :($p == $v.$n))
+           end
 
   if(e1.head == :(->))
    (p,c) = e1.args
-   if(p.head == :(call))
+   if(isa(p,Symbol))
+     :(let $p = $v ; $c end)
+   elseif(!isa(p,Expr))
+     :(if($p == $v) ; $c else $np end)
+   elseif(p.head == :(call))
      t = eval(p.args[1])
      es = map(spat, t.names, p.args[2:end])
      :(if(isa($v,$t)) ; $(pcomp(c,np,es)) else $np end)
+   elseif(p.head == :(::))
+     (vn,t) = p.args
+     :(if(isa($v,$t)) ; let $vn = $v ; $c end else ; $np end)
    end
   end
  end
